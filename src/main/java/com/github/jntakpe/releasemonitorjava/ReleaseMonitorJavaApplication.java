c5a831c503b50900ff59bb9969b0ac808bfb91df
jntakpe <jntakpe@gmail.com>
2017-10-08 21:48:13 +0200

    Gradle configuration

---
+++ src/main/java/com/github/jntakpe/releasemonitorjava/ReleaseMonitorJavaApplication.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.releasemonitorjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseMonitorJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseMonitorJavaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/jntakpe/releasemonitorjava/ReleaseMonitorJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.jntakpe.releasemonitorjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ReleaseMonitorJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
